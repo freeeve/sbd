9004f36cb6720ae44cac7a1dd57aef95cca4c289
Wes Freeman <wes@skeweredrook.com>
2014-06-24 17:03:52 -0400

    still missing a couple

--- tokenize.go
+++ tokenize.go
@@ -1,50 +1,112 @@
 package sbd
 
 import (
+	"fmt"
 	"regexp"
+	"strings"
 )
 
 func uniformQuotes(s string) string {
-	// uniform quotes
+	//(re.compile(r'\'\''), r'"'),
+	//(re.compile(r'\`\`'), r'"'),
 	s = regexp.MustCompile("''").ReplaceAllString(s, "\"")
 	s = regexp.MustCompile("``").ReplaceAllString(s, "\"")
-   return s
+	return s
 }
 
 func separatePunctuation(s string) string {
-	return s
+	// it's supposed to do this. it might not.
+	//(re.compile(r'(^|\s)(\')'), r'\1\2 '),
+	//(re.compile(r'(?=[\(\"\`{\[:;&\#\*@])(.)'), r'\1 '),
+
+	//(re.compile(r'(.)(?=[?!)\";}\]\*:@\'])'), r'\1 '),
+	//(re.compile(r'(?=[\)}\]])(.)'), r'\1 '),
+	//(re.compile(r'(.)(?=[({\[])'), r'\1 '),
+	//(re.compile(r'((^|\s)\-)(?=[^\-])'), r'\1 '),
+	r := strings.NewReader(s)
+	retVal := ""
+	justSpaced := false
+	for c, _, err := r.ReadRune(); err == nil; c, _, err = r.ReadRune() {
+		switch c {
+		case ':', ';', '&', '\\', '/', '(', ')', '#', '*', '@', '\'', '{', '}', '-', '%':
+			retVal = fmt.Sprintf("%s %c", retVal, c)
+			justSpaced = true
+		default:
+			if justSpaced {
+				retVal = fmt.Sprintf("%s ", retVal)
+				justSpaced = false
+			}
+			retVal = fmt.Sprintf("%s%c", retVal, c)
+		}
+	}
+	return retVal
 }
 func doubleHyphenToOne(s string) string {
+	//(re.compile(r'([^-])(\-\-+)([^-])'), r'\1 \2 \3'),
+	//(re.compile(r'(\s|^)(,)(?=(\S))'), r'\1\2 '),
+	s = regexp.MustCompile("([^-])(\\-\\-+)([^-])").ReplaceAllString(s, "$1 $2 $3")
+	s = regexp.MustCompile("(\\-)\\s+(\\-)").ReplaceAllString(s, "$1$2")
 	return s
 }
 
 func commaSpaceFollows(s string) string {
+	//Only separate comma if space follows:
+	//(re.compile(r'(.)(,)(\s|$)'), r'\1 \2\3'),
+	s = regexp.MustCompile("(.)(,)(\\s|$)").ReplaceAllString(s, "$1 $2$3")
 	return s
 }
 
 func combineDots(s string) string {
+	//# Combine dots separated by whitespace to be a single token:
+	//(re.compile(r'\.\s\.\s\.'), r'...'),
+	s = regexp.MustCompile("\\.\\s\\.\\s\\.").ReplaceAllString(s, "...")
 	return s
 }
 func separateNoNum(s string) string {
+	//# Separate "No.6"
+	//(re.compile(r'([A-Za-z]\.)(\d+)'), r'\1 \2'),
+	s = regexp.MustCompile("([A-Za-z]\\.)(\\d+)").ReplaceAllString(s, "$1 $2")
 	return s
 }
 func separateWordsFromEllipses(s string) string {
+	//# Separate words from ellipses
+	//(re.compile(r'([^\.]|^)(\.{2,})(.?)'), r'\1 \2 \3'),
+	//(re.compile(r'(^|\s)(\.{2,})([^\.\s])'), r'\1\2 \3'),
+	//(re.compile(r'([^\.\s])(\.{2,})($|\s)'), r'\1 \2\3'),
+	s = regexp.MustCompile("")
 	return s
 }
 
 func fixPercentDollarAmpersand(s string) string {
+	//# fix %, $, &
+	//(re.compile(r'(\d)%'), r'\1 %'),
+	//(re.compile(r'\$(\.?\d)'), r'$ \1'),
+	//(re.compile(r'(\w)& (\w)'), r'\1&\2'),
+	//(re.compile(r'(\w\w+)&(\w\w+)'), r'\1 & \2'),
+	// is this necessary ?
+	s = regexp.MustCompile("(\\w)& (\\w)").ReplaceAllString(s, "$1&$2")
+	s = regexp.MustCompile("(\\w\\w+)&(\\w\\w+)").ReplaceAllString(s, "$1 & $2")
 	return s
 }
 
 func fixNt(s string) string {
+	//# fix (n 't) --> ( n't)
+	//(re.compile(r'n \'t( |$)'), r" n't\1"),
+	//(re.compile(r'N \'T( |$)'), r" N'T\1"),
+
 	return s
 }
 
 func separateCant(s string) string {
+	//# treebank tokenizer special words
+	//(re.compile(r'([Cc])annot'), r'\1an not'),
+
 	return s
 }
 
 func compactSpaces(s string) string {
+	//# compact spaces into just one
+	//(re.compile(r'\s+'), r' '),
 	return s
 }
 
@@ -62,61 +124,3 @@ func tokenize(s string) string {
 	s = compactSpaces(s)
 	return s
 }
-
-/*
-"""
-A list of (regexp, repl) pairs applied in sequence.
-The resulting string is split on whitespace.
-(Adapted from the Punkt Word Tokenizer)
-"""
-
-_tokenize_regexps = [
-
-    # uniform quotes
-    (re.compile(r'\'\''), r'"'),
-    (re.compile(r'\`\`'), r'"'),
-
-    # Separate punctuation (except period) from words:
-    (re.compile(r'(^|\s)(\')'), r'\1\2 '),
-    (re.compile(r'(?=[\(\"\`{\[:;&\#\*@])(.)'), r'\1 '),
-
-    (re.compile(r'(.)(?=[?!)\";}\]\*:@\'])'), r'\1 '),
-    (re.compile(r'(?=[\)}\]])(.)'), r'\1 '),
-    (re.compile(r'(.)(?=[({\[])'), r'\1 '),
-    (re.compile(r'((^|\s)\-)(?=[^\-])'), r'\1 '),
-
-    # Treat double-hyphen as one token:
-    (re.compile(r'([^-])(\-\-+)([^-])'), r'\1 \2 \3'),
-    (re.compile(r'(\s|^)(,)(?=(\S))'), r'\1\2 '),
-
-    # Only separate comma if space follows:
-    (re.compile(r'(.)(,)(\s|$)'), r'\1 \2\3'),
-
-    # Combine dots separated by whitespace to be a single token:
-    (re.compile(r'\.\s\.\s\.'), r'...'),
-
-    # Separate "No.6"
-    (re.compile(r'([A-Za-z]\.)(\d+)'), r'\1 \2'),
-
-    # Separate words from ellipses
-    (re.compile(r'([^\.]|^)(\.{2,})(.?)'), r'\1 \2 \3'),
-    (re.compile(r'(^|\s)(\.{2,})([^\.\s])'), r'\1\2 \3'),
-    (re.compile(r'([^\.\s])(\.{2,})($|\s)'), r'\1 \2\3'),
-
-    ## adding a few things here:
-
-    # fix %, $, &
-    (re.compile(r'(\d)%'), r'\1 %'),
-    (re.compile(r'\$(\.?\d)'), r'$ \1'),
-    (re.compile(r'(\w)& (\w)'), r'\1&\2'),
-    (re.compile(r'(\w\w+)&(\w\w+)'), r'\1 & \2'),
-
-    # fix (n 't) --> ( n't)
-    (re.compile(r'n \'t( |$)'), r" n't\1"),
-    (re.compile(r'N \'T( |$)'), r" N'T\1"),
-
-    # treebank tokenizer special words
-    (re.compile(r'([Cc])annot'), r'\1an not'),
-
-    (re.compile(r'\s+'), r' '),
-*/
--- tokenize_test.go
+++ tokenize_test.go
@@ -12,12 +12,75 @@ type TokenizeSuite struct{}
 
 var _ = Suite(&TokenizeSuite{})
 
-func (s *TokenizeSuite) TestTokenize(c *C) {
+func (s *TokenizeSuite) TestUniformQuotes(c *C) {
 	// uniform quotes
-	c.Assert(tokenize("''hello''"), Equals, "\"hello\"")
-	c.Assert(tokenize("``hello``"), Equals, "\"hello\"")
+	c.Assert(uniformQuotes("''hello''"), Equals, "\"hello\"")
+	c.Assert(uniformQuotes("``hello``"), Equals, "\"hello\"")
+}
 
+func (s *TokenizeSuite) TestSeparatePunctuation(c *C) {
 	// separate punctuation (except period) from words
-	c.Assert(tokenize("hello:"), Equals, "hello :")
-	c.Assert(tokenize("hello:wut"), Equals, "hello : wut")
+	c.Assert(separatePunctuation("hello:"), Equals, "hello :")
+	c.Assert(separatePunctuation("hello:wut"), Equals, "hello : wut")
+	c.Assert(separatePunctuation("hello;"), Equals, "hello ;")
+	c.Assert(separatePunctuation("hello;wut"), Equals, "hello ; wut")
+	c.Assert(separatePunctuation("hello&"), Equals, "hello &")
+	c.Assert(separatePunctuation("hello&wut"), Equals, "hello & wut")
+	c.Assert(separatePunctuation("hello\\"), Equals, "hello \\")
+	c.Assert(separatePunctuation("hello\\wut"), Equals, "hello \\ wut")
+	c.Assert(separatePunctuation("hello/"), Equals, "hello /")
+	c.Assert(separatePunctuation("hello/wut"), Equals, "hello / wut")
+	c.Assert(separatePunctuation("hello("), Equals, "hello (")
+	c.Assert(separatePunctuation("hello(wut"), Equals, "hello ( wut")
+	c.Assert(separatePunctuation("hello)"), Equals, "hello )")
+	c.Assert(separatePunctuation("hello)wut"), Equals, "hello ) wut")
+	c.Assert(separatePunctuation("hello#"), Equals, "hello #")
+	c.Assert(separatePunctuation("hello#wut"), Equals, "hello # wut")
+	c.Assert(separatePunctuation("hello*"), Equals, "hello *")
+	c.Assert(separatePunctuation("hello*wut"), Equals, "hello * wut")
+	c.Assert(separatePunctuation("hello@"), Equals, "hello @")
+	c.Assert(separatePunctuation("hello@wut"), Equals, "hello @ wut")
+	c.Assert(separatePunctuation("hello'"), Equals, "hello '")
+	c.Assert(separatePunctuation("hello'wut"), Equals, "hello ' wut")
+	c.Assert(separatePunctuation("hello{"), Equals, "hello {")
+	c.Assert(separatePunctuation("hello{wut"), Equals, "hello { wut")
+	c.Assert(separatePunctuation("hello}"), Equals, "hello }")
+	c.Assert(separatePunctuation("hello}wut"), Equals, "hello } wut")
+	c.Assert(separatePunctuation("hello-"), Equals, "hello -")
+	c.Assert(separatePunctuation("hello-wut"), Equals, "hello - wut")
+	c.Assert(separatePunctuation("hello%"), Equals, "hello %")
+	c.Assert(separatePunctuation("hello%wut"), Equals, "hello % wut")
+}
+
+func (s *TokenizeSuite) TestDoubleHyphenToOne(c *C) {
+	c.Assert(doubleHyphenToOne("hello--wut"), Equals, "hello -- wut")
+}
+
+func (s *TokenizeSuite) TestCommaSpaceFollows(c *C) {
+	c.Assert(commaSpaceFollows("hello,wut"), Equals, "hello,wut")
+	c.Assert(commaSpaceFollows("hello, wut"), Equals, "hello , wut")
+}
+
+func (s *TokenizeSuite) TestCombineDots(c *C) {
+	c.Assert(combineDots(". . ."), Equals, "...")
+}
+
+func (s *TokenizeSuite) TestSeparateNoNum(c *C) {
+	c.Assert(separateNoNum("No.6"), Equals, "No. 6")
+}
+
+func (s *TokenizeSuite) TestSeparateWordsFromEllipses(c *C) {
+	c.Assert(separateWordsFromEllipses("hello..."), Equals, "hello ...")
+}
+
+func (s *TokenizeSuite) TestFixPercentDollarAmpersand(c *C) {
+	c.Assert(fixPercentDollarAmpersand("hello& wut"), Equals, "hello & wut")
+	c.Assert(fixPercentDollarAmpersand("hello&wut"), Equals, "hello & wut")
+}
+
+func (s *TokenizeSuite) TestTokenize(c *C) {
+	c.Assert(tokenize("hello--wut"), Equals, "hello -- wut")
+	c.Assert(tokenize("hello--wut--foo"), Equals, "hello -- wut -- foo")
+	c.Assert(tokenize("60%"), Equals, "60 %")
+	c.Assert(tokenize("hello..."), Equals, "hello ...")
 }
